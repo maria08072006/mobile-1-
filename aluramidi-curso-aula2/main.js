2e347335be68d42c0d17d669dbb87eceb086d1aa
maria08072006 <130079102+maria08072006@users.noreply.github.com>
2023-09-28 13:50:42 -0300

    Add files via upload

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -1,17 +1,24 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomPom;
-
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas= document.querySelectorAll('.tecla');
+    
+let contador= 0;
+    
+while (contador<listaDeTeclas.length) {
+    
+    const tecla = listaDeTeclas[contador];
+    
+    const instrumento = tecla.classList [1];
+    
+    const idAudio =`#som_${instrumento}`;
+    
+        console.log (idAudio);
+    
+    tecla.onclick= function () {
+        tocaSom(idAudio);
+    }
+    contador=contador+1;
+    
+    console.log(contador);
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
